5840130adfdc13b81f352ce4c85270d1a9927641
GulNabbievva <114688119+GulNabbievva@users.noreply.github.com>
2022-10-20 20:48:36 +0400

    Create JS TASK_2.js

---
+++ JS TASK_2.js
@@ -0,0 +1,7 @@
+function minimumElement(array){
+   return array = Math.min.apply(null,array);
+    }
+   
+const sample_array = [1,2,4,6,71,2,4,9,100];
+
+console.log(minimumElement(sample_array));
